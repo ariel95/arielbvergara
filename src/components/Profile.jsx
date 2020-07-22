f756f64906ab7513381d1b8ceb3eb2938f6c93c1
arielvergarautn <arie@est.frba.utn.edu.ar>
2020-07-21 23:26:06 -0300

    firebase

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,19 +4,6 @@ import '../public/css/profile.css'
 
 const Profile = () => {
     return (
-        // <div id="profile" className="card mb-3">
-        //     <div className="row no-gutters">
-        //         <div className="col-lg-5 col-md-4 text-center">
-        //             <img src={profilePicture} className="card-img" alt="..." />
-        //         </div>
-        //         <div className="col-lg-7 col-md-8" style={{ display: "flex", alignItems: "center" }}>
-        //             <div className="card-body">
-        //                 <h1 className="card-title title">Ariel Bernardo Vergara</h1>
-        //                 <h3 className="card-text subtitle">Desarrollador Full Stack .NET</h3>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
         <section id="profile">
             <div className="row no-gutters">
                 <div className="col-lg-4 col-md-4 col-xs-12">
